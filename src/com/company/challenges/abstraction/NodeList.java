20e848a2dead5dc8e28cb0ab9aa2c499e743370f
cerenercan <36184074+cerenercan@users.noreply.github.com>
2021-09-29 13:27:44 +0300

    created an interface.

---
+++ src/com/company/challenges/abstraction/NodeList.java
@@ -0,0 +1,9 @@
+package com.company.challenges.abstraction;
+
+public interface NodeList {
+
+    ListItem getRoot();
+    boolean addItem(ListItem item);
+    boolean removeItem(ListItem item);
+    void traverse(ListItem item);
+}
