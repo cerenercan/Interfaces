efed89b3f2fe3cde65f22487f113a26ee609e545
cerenercan <36184074+cerenercan@users.noreply.github.com>
2021-09-29 15:13:29 +0300

    last updates

--- src/com/company/challenges/abstraction/MainAbstraction.java
+++ src/com/company/challenges/abstraction/MainAbstraction.java
@@ -13,28 +13,28 @@ public class MainAbstraction {
         for (String s : data){
             list.addItem(new Node(s));
         }
+
+
+        list.traverse(list.getRoot());
+        list.removeItem(new Node("3"));
         list.traverse(list.getRoot());
 
-//        list.traverse(list.getRoot());
-//        list.removeItem(new Node("3"));
-//        list.traverse(list.getRoot());
-//
-//        list.removeItem(new Node("5"));
-//        list.traverse(list.getRoot());
-//
-//        list.removeItem(new Node("0"));
-//        list.removeItem(new Node("4"));
-//        list.removeItem(new Node("2"));
-//        list.traverse(list.getRoot());
-//
-//        list.removeItem(new Node("9"));
-//        list.traverse(list.getRoot());
-//        list.removeItem(new Node("8"));
-//        list.traverse(list.getRoot());
-//        list.removeItem(new Node("6"));
-//        list.traverse(list.getRoot());
-//        list.removeItem(list.getRoot());
-//        list.traverse(list.getRoot());
+        list.removeItem(new Node("5"));
+        list.traverse(list.getRoot());
+
+        list.removeItem(new Node("0"));
+        list.removeItem(new Node("4"));
+        list.removeItem(new Node("2"));
+        list.traverse(list.getRoot());
+
+        list.removeItem(new Node("9"));
+        list.traverse(list.getRoot());
+        list.removeItem(new Node("8"));
+        list.traverse(list.getRoot());
+        list.removeItem(new Node("6"));
+        list.traverse(list.getRoot());
+        list.removeItem(list.getRoot());
+        list.traverse(list.getRoot());
 
 
 
